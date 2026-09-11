f97e3777023c8a27786ad6fa69cf20f96751e398
Archit Awasthi <167566400+architawasthi99@users.noreply.github.com>
2026-09-11 23:57:13 +0530

    Add solution for counting unique 3-digit even numbers
    Implement a solution to count unique 3-digit even numbers using given digits.

---
+++ September 2026/04_3483. Unique 3-Digit Even Numbers.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+        int freq[10] = {};
+
+        for (int d : digits)
+            freq[d]++;
+
+        int ans = 0;
+
+        for (int a = 1; a <= 9; a++) {
+            for (int b = 0; b <= 9; b++) {
+                for (int c = 0; c <= 8; c += 2) {
+
+                    // Temporarily use the digits
+                    if (freq[a] == 0)
+                        continue;
+
+                    freq[a]--;
+
+                    if (freq[b] == 0) {
+                        freq[a]++;
+                        continue;
+                    }
+
+                    freq[b]--;
+
+                    if (freq[c] > 0)
+                        ans++;
+
+                    freq[b]++;
+                    freq[a]++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
