321dfa4a15054986246fda11a5129ff749f60a5f
Archit Awasthi <167566400+architawasthi99@users.noreply.github.com>
2026-09-13 23:13:11 +0530

    Add Solution class for largest image overlap problem

---
+++ September 2026/06_835. Image Overlap.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    struct Hash {
+        size_t operator()(const pair<int,int>&p)const {
+            return p.first*100000+p.second;
+        }
+    };
+    
+    int largestOverlap(vector<vector<int>>& img1,vector<vector<int>>& img2) {
+        int n=img1.size(),ans=0;
+        unordered_map<pair<int,int>,int,Hash> mp;
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+                if(img1[i][j])
+                    for(int x=0;x<n;x++)
+                        for(int y=0;y<n;y++)
+                            if(img2[x][y])
+                                ans=max(ans,++mp[{i-x,j-y}]);
+        return ans;
+    }
+};
